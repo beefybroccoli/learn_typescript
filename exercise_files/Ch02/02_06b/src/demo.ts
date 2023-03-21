748cc2562a7a7e34f8895219821088346ed1f5ed
beefybroccoli <tedkimemail@yahoo.com>
2023-03-20 23:53:39 -0700

    ...

--- exercise_files/Ch02/02_06b/src/demo.ts
+++ exercise_files/Ch02/02_06b/src/demo.ts
@@ -1,11 +1,24 @@
 interface Contact {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 function clone(source: Contact): Contact {
-    return Object.apply({}, source);
+  return Object.apply({}, source);
+}
+
+function clone_generic<T>(source: T): T {
+  return Object.apply({}, source);
+}
+
+function clone_generic_2_types<T1, T2>(source: T1): T2 {
+  return Object.apply({}, source);
 }
 
 const a: Contact = { id: 123, name: "Homer Simpson" };
-const b = clone(a)
+const b = clone(a);
+const b1 = clone_generic<Contact>(a);
+const b2 = clone_generic_2_types<Contact, Contact>(a);
+
+const aa = { id: 123, name: "Homer Simpson" };
+const bb = clone(aa);
--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,27 +1,49 @@
+//Check solution here
+
+enum Status {
+  InProgress = "in-progress",
+  ToDo = "todo",
+  Done = "done",
+}
+
+interface ToDo {
+  id: number;
+  title: String;
+  status: Status;
+  completedOn?: Date;
+}
+
 const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
-]
+  {
+    id: 1,
+    title: "Learn HTML",
+    status: Status.Done,
+    completedOn: new Date("2021-09-11"),
+  },
+  { id: 2, title: "Learn TypeScript", status: Status.InProgress },
+  { id: 3, title: "Write the best app in the world", status: Status.ToDo },
+];
 
 function addTodoItem(todo) {
-    const id = getNextId(todoItems)
+  const id = getNextId(todoItems);
 
-    const newTodo = {
-        id,
-        title: todo,
-        status: "todo",
-    }
+  const newTodo = {
+    id,
+    title: todo,
+    status: "todo",
+  };
 
-    todoItems.push(newTodo)
+  todoItems.push(newTodo);
 
-    return newTodo
+  return newTodo;
 }
 
 function getNextId(items) {
-    return items.reduce((max, x) => x.id > max ? max : x.id, 0) + 1
+  return items.reduce((max, x) => (x.id > max ? max : x.id), 0) + 1;
 }
 
-const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+const newTodo = addTodoItem(
+  "Buy lots of stuff with all the money we make from the app"
+);
 
-console.log(JSON.stringify(newTodo))
+console.log(JSON.stringify(newTodo));
