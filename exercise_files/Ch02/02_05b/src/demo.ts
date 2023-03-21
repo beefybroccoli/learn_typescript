0d829600c6709f12ce4a0b9d0907bd298c7b00ff
beefybroccoli <tedkimemail@yahoo.com>
2023-03-20 23:26:55 -0700

    ...

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,28 @@
 interface Contact {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
+  method_1(input: string): Contact;
+  method_2(): Contact;
 }
 
-function clone(source) {
-    return Object.apply({}, source);
+/**
+ *
+ * @param source
+ * @returns
+ */
+function clone(source: Contact): Contact {
+  return Object.apply({}, source);
 }
+
+/**
+ *
+ * @param source
+ * @param func
+ * @returns
+ */
+function clone_2(source: Contact, func: (source: Contact) => Contact): Contact {
+  return Object.apply({}, source);
+}
+
+const a: Contact = { id: 123, name: "Tom" };
+const b = clone(a);
