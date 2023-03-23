c2359679685ac9875191d00cca8f3f6ed93004c5
beefybroccoli <tedkimemail@yahoo.com>
2023-03-23 08:15:15 -0700

    ...

--- mastering_typescript/ch01/hello_typescript.js
+++ mastering_typescript/ch01/hello_typescript.js
@@ -1,4 +1,7 @@
-var version = "es6";
-console.log("hello ".concat(version, " TypeScript"));
+"use strict";
+var version = `es6`;
+console.log(`hello ${version} TypeScript`);
 console.log("hello " + version + " TypeScript");
-console.log("some  change");
+console.log("some  change here");
+var number_1 = 1;
+console.log("number_1 = " + number_1);
--- mastering_typescript/ch01/hello_typescript.ts
+++ mastering_typescript/ch01/hello_typescript.ts
@@ -1,4 +1,6 @@
-var version = `es6`;
+var version: string = `es6`;
 console.log(`hello ${version} TypeScript`);
 console.log("hello " + version + " TypeScript");
-console.log("some  change");
+console.log("some  change here");
+var number_1: number = 1;
+console.log("number_1 = " + number_1);
