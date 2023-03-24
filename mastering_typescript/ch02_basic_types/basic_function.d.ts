6c1e73351fe8e0d20820407ff59a8cf7c2df2891
ted <tedkimemail@yahoo.com>
2023-03-23 19:36:13 -0700

    ...

---
+++ mastering_typescript/ch02_basic_types/basic_function.d.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param a
+ * @param b
+ * @param c
+ */
+declare function calculate_return_void(a: number, b: number, c: number): void;
+/**
+ *
+ * @param a
+ * @param b
+ * @param c
+ * @returns
+ */
+declare function calculate_return_number(a: number, b: number, c: number): number;
+/**
+ *
+ * @param input
+ */
+declare function printString_return_void(input: string): void;
---
+++ mastering_typescript/ch02_basic_types/basic_types.d.ts
@@ -0,0 +1,7 @@
+declare var myBoolean: boolean;
+declare var myNumber: number;
+declare var myString: string;
+declare var myStringArray: string[];
+declare var inferredVar1: string;
+declare var inferredVar2: number;
+declare var inferredVar3: boolean;
